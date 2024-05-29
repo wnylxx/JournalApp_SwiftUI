9b6f3a2ce1097e137b2477a69fd4f3f23a3d26ff
WonyeolHEO <hyy96hyy@naver.com>
2024-05-29 22:26:41 +0900

    initial Setting

---
+++ SwiftUI_JNRL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_JNRL
+//
+//  Created by wonyoul heo on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_JNRL/SwiftUI_JNRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JNRLApp.swift
+//  SwiftUI_JNRL
+//
+//  Created by wonyoul heo on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JNRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
