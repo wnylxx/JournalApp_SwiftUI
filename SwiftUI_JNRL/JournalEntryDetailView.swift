44aae2f97b8bd1642f7a312065cb1c3d89fec66b
WonyeolHEO <hyy96hyy@naver.com>
2024-06-02 22:52:59 +0900

    snapshot 기능 추가

--- SwiftUI_JNRL/JournalEntryDetailView.swift
+++ SwiftUI_JNRL/JournalEntryDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct JournalEntryDetailView: View {
     var journalEntry: JournalEntry
@@ -43,6 +44,31 @@ struct JournalEntryDetailView: View {
         }
         .navigationTitle("Entry Detail")
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            getMapSnapshot()
+        }
+    }
+    
+    private func getMapSnapshot() {
+        if let lat = journalEntry.latitude,
+           let long = journalEntry.longitude {
+            let options = MKMapSnapshotter.Options()
+            options.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long),
+                                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            options.size = CGSize(width: 300, height: 300)
+            options.preferredConfiguration = MKStandardMapConfiguration()
+            
+            let snapShotter = MKMapSnapshotter(options: options)
+            snapShotter.start{ snapshot, error in
+                if let snap = snapshot {
+                    mapImage = snap.image
+                    } else if let error = error {
+                        print("error: \(error.localizedDescription)")
+                    }
+            }
+        } else {
+            mapImage = UIImage(systemName: "map")
+        }
     }
 }
 
--- SwiftUI_JNRL/justTest.swift
+++ SwiftUI_JNRL/justTest.swift
@@ -5,4 +5,3 @@
 //  Created by wonyoul heo on 5/29/24.
 //
 
-
